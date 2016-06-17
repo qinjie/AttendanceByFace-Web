99ea5234bdfd95cd6abf24ebe6108a7792da6b41
canhnht <canhnht1709@gmail.com>
2016-06-17 09:28:55 +0800

    Update db

--- docs/student_attendance.sql
+++ docs/student_attendance.sql
@@ -314,8 +314,8 @@ CREATE TABLE `venue_beacon` (
 
 insert into `venue_beacon` (`id`, `venue_id`, `beacon_id`) values
   (1, 1, 1),
-  (2, 2, 2),
-  (3, 3, 3);
+  (2, 2, 1),
+  (3, 3, 1);
 
 /*Data for the table `venue_beacon` */
 
