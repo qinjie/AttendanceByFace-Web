d0545683219469c84038e4922aa1eb39533d057f
canhnht <canhnht1709@gmail.com>
2016-07-19 16:34:56 +0800

    Update API for register device

--- cronjobs/start_device_activation.sh
+++ cronjobs/start_device_activation.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-python ./device_activation.py >> ./log_device_activation.txt 2>& 1
+# python ./device_activation.py >> ./log_device_activation.txt 2>&1
+
+# silent output
+python ./device_activation.py > /dev/null 2>&1
