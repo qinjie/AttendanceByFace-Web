85078d9e7451db02020680466ee2b89f04526adb
canhnht <canhnht1709@gmail.com>
2016-10-10 15:50:37 +0800

    Update test for API attendance history

--- tests/codeception/api/functional/AttendanceCest.php
+++ tests/codeception/api/functional/AttendanceCest.php
@@ -57,4 +57,45 @@ class AttendanceCest
             ]
         ], '$[*]');
     }
+
+    public function getAttendanceHistory_ForAClass_InSemester(FunctionalTester $I)
+    {
+        $I->wantTo('get attendance history for a class in whole semester');
+        $I->sendGET('v1/attendance/history', [
+            'class_section' => 'T1M2',
+            'expand' => 'lesson'
+        ]);
+        $I->seeResponseCodeIs(200);
+        $userId = $I->grabFromDatabase('user', 'id', [
+            'username' => 'canhnht'
+        ]);
+        $studentId = $I->grabFromDatabase('student', 'id', [
+            'user_id' => $userId
+        ]);
+        $fromDate = Attendance::SEMESTER_START_DATE;
+        $toDate = Attendance::SEMESTER_END_DATE;
+        $response = json_decode($I->grabResponse());
+        foreach ($response as $item) {
+            $I->assertEquals($studentId, $item->student_id);
+            $I->assertEquals('T1M2', $item->lesson->class_section);
+            $I->assertGreaterThanOrEqual($fromDate, $item->recorded_date);
+            $I->assertLessThanOrEqual($toDate, $item->recorded_date);
+        }
+        $I->seeResponseMatchesJsonType([
+            'id' => 'integer',
+            'student_id' => 'string',
+            'lesson_id' => 'integer',
+            'recorded_date' => 'string',
+            'lesson' => [
+                'id' => 'integer',
+                'semester' => 'string',
+                'module_id' => 'string',
+                'venue_id' => 'integer',
+                'weekday' => 'string',
+                'start_time' => 'string',
+                'end_time' => 'string',
+                'meeting_pattern' => 'string'
+            ]
+        ], '$[*]');
+    }
 }
